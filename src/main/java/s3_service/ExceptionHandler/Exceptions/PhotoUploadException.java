a1ec8ff175b2d168060e67e98c1ea9132f1164f8
phyphloran <mareeviv@yandex.ru>
2026-01-04 20:31:37 +0300

    init commit

--- src/main/java/s3_service/ExceptionHandler/Exceptions/PhotoUploadException.java
+++ src/main/java/s3_service/ExceptionHandler/Exceptions/PhotoUploadException.java
@@ -1,6 +1,5 @@
 package s3_service.ExceptionHandler.Exceptions;
 
-
 public class PhotoUploadException extends RuntimeException {
     public PhotoUploadException(String message) {
         super(message);
